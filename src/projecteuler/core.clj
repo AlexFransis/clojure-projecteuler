7a09487a9029129de2157d549774da9780f0e78e
AlexFransis <Ghost@ghost.local>
2016-11-30 00:49:47 -0500

    Refactored and improved problem 8

--- src/projecteuler/core.clj
+++ src/projecteuler/core.clj
@@ -28,11 +28,6 @@
        (take n)
        (last)))
 
-(defn square
-  "Squares the number"
-  [x]
-  (* x x))
-
 (defn palindrome?
   "Checks if the number is a palindrome"
   [n]
@@ -111,43 +106,17 @@
 
 (def large-number 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450)
 
-(defn drop-first
-  "Drops the first number in a series and returns the series"
-  [x]
-  (as-> x n
-       (str n)
-       (seq n)
-       (rest n)
-       (apply str n)
-       (if-not (clojure.string/blank? n)
-         (bigint n)
-         n)))
+(defn digits-of
+  "Takes a number and returns a list of all its digits"
+  [n]
+  (map #(Integer/parseInt (str %)) (str n)))
 
-(defn digit-product
-  "Returns the product of its digits"
-  [x]
-  (->> x
-       (str)
-       (seq)
-       (map #(Character/digit % 10))
-       (reduce *)))
+(->> large-number
+     (digits-of)
+     (partition 13 1)
+     (map #(reduce * %))
+     (apply max))
 
-(defn current-digits
-  "Takes a large number and returns the first 13 digits"
-  [x]
-  (->> x
-       (str)
-       (seq)
-       (take 13)
-       (apply str)
-       (Long.)))
-
-(loop [x 0
-       y large-number]
-  (if-not (clojure.string/blank? (str y))
-    (recur (if (< (digit-product x)
-                  (digit-product (current-digits y)))
-             (current-digits y)
-             x)
-           (drop-first y))
-    (digit-product x)))
+;;---------------------------------------------------------------------
+;; Problem 9 -- Special Pythagorean Triplet
+;;---------------------------------------------------------------------
